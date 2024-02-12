2c28fc037ab996ddba357c5e6304a9c1878ed5a6
BetterBite <38665746+BetterBite@users.noreply.github.com>
2024-02-12 15:45:40 +0000

    Added basic network ownership mechanics
    Not tested to see if it actually works!

--- GoudaNugh/Assets/Scripts/NetworkedXRInteractible.cs
+++ GoudaNugh/Assets/Scripts/NetworkedXRInteractible.cs
@@ -12,29 +12,43 @@ public class NetworkedXRInteractible : MonoBehaviour
     {
         _grabInteractible = GetComponent<XRGrabInteractable>();
         _networkObject = GetComponent<NetworkObject>();
-        _grabInteractible.selectEntered.AddListener(LogSelect);
+        _grabInteractible.selectEntered.AddListener(ChangeOwnerOnSelect);
+        _grabInteractible.selectExited.AddListener(RelinquishObjectOnLetgo);
     }
 
-    private void LogSelect(SelectEnterEventArgs args)
+    // Changes owner upon the object being grabbed.
+    // IXRSelectInteractor does not have a method to obtain its NetworkObject component thus it needs to be cast to MonoBehaviour
+    private void ChangeOwnerOnSelect(SelectEnterEventArgs args)
     {
-        Debug.Log("Selected");
+        //Debug.Log("Selected");
         NetworkObject interactorNetworkObject;
         MonoBehaviour interactor = args.interactorObject as MonoBehaviour;
         if (interactor != null) {
-            Debug.Log("Interactor found");
+            //Debug.Log("Interactor found");
             interactorNetworkObject = interactor.GetComponent<NetworkObject>();
-            Debug.Log(interactorNetworkObject);
+            //Debug.Log(interactorNetworkObject);
             if (interactorNetworkObject != null) {
                 if (_networkObject.IsOwner) {
                     Debug.Log("Owner selected");
                 }
                 else {
                     Debug.Log("Non-owner selected");
+                    _networkObject.ChangeOwnership(interactorNetworkObject.OwnerClientId);
                 }
             }
             else {
                 Debug.LogError(string.Format("Could not find NetworkObject for the interactor! Interactor is: {0} and found NetworkObject: {1}", args.interactorObject.ToString(), interactorNetworkObject));
             }
+        }
+    }
+
+    private void RelinquishObjectOnLetgo(SelectExitEventArgs args) 
+    {
+        if (_networkObject.IsOwner) {
+            Debug.Log("Client relinquished object");
+            _networkObject.RemoveOwnership();
+        } else {
+            Debug.Log("Server let go of the object");
         }
     }
 }
