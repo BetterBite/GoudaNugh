59144271bc71177c720c1967d0f1958bce3fa6cf
jacobrseaborn <jacobrseaborn@gmail.com>
2024-03-12 13:04:10 +0000

    set up response handlers to respond to cues

--- GoudaNugh/Assets/WitnessHandler.cs
+++ GoudaNugh/Assets/WitnessHandler.cs
@@ -13,6 +13,7 @@ public class WitnessHandler : MonoBehaviour
 {
     // audioClips contains all the possible voice clips said by witness
     public AudioClip[] audioClips;
+    public AudioSource audioSource;
     public Oculus.Voice.AppVoiceExperience appVoiceExperience;
     public TextMeshProUGUI subtitle;
 
@@ -49,5 +50,44 @@ public class WitnessHandler : MonoBehaviour
 
     public void HandleIntents(string[] values) {
         Debug.Log("WIT output:"+ string.Join(", ", values));
+
+        if (values[0] == "info_about_self" && values.Length > 1) {
+            if (values[1] == "name") {
+                // play name audio
+                PlayAudio(audioClips[0]);
+            } else if (values[3] == "victim") {
+                // play found victim audio
+                PlayAudio(audioClips[1]);
+            } else if (values[2] == "alibi") {
+                // play alibi audio
+                PlayAudio(audioClips[2]);
+            } else if (values[1] == "thomas") {
+                PlayAudio(audioClips[4]);
+            } else if (values[1] == "lurni") {
+                PlayAudio(audioClips[5]);
+            } else if (values[1] == "walters") {
+                PlayAudio(audioClips[6]);
+            }
+        } else if (values[0] == "info_about_item" && values.Length > 1) {
+            if (values[1] == "figurine") {
+                // play figurine audio
+                PlayAudio(audioClips[3]);
+            } else if (values[2] == "clock") {
+                // play clock audio
+                PlayAudio(audioClips[4]);
+            } else if (values[3] == "globe") {
+                // play globe audio
+                PlayAudio(audioClips[5]);
+            } else if (values[4] == "telescope") {
+                // play telescope audio
+                PlayAudio(audioClips[6]);
+            }
+        }        
+    }
+
+    private void PlayAudio(AudioClip clip) {
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 }
