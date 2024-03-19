a05361dddcbcc0c7130f8aa5c0f24b7ed606e84c
BetterBite <38665746+BetterBite@users.noreply.github.com>
2024-03-19 14:20:47 +0000

    fully remove delegation :(

--- GoudaNugh/Assets/Network Assets/InteractibleManager.cs
+++ GoudaNugh/Assets/Network Assets/InteractibleManager.cs
@@ -9,13 +9,6 @@ using System;
 public class InteractibleManager : NetworkBehaviour {
     public static InteractibleManager Instance { get; private set; }
     public List<GameObject> ObjectsToSpawn;
-    // Idea of using a delegate to cut on massive if-else statements was suggested by GitHub Copilot Chat. Thanks GitHub Copilot Chat
-    private delegate GameObject InstantiateDelegate(PastObject pastObject, NetworkObject networkObject);
-    private readonly Dictionary<Type, InstantiateDelegate> instantiateObject = new Dictionary<Type, InstantiateDelegate> {
-        { typeof(PastObject), InstantiatePastObject },
-        { typeof(FutureObject), InstantiateFutureObject },
-    };
-
 
     public void Awake() {
         Instance = this;
@@ -53,84 +46,6 @@ public class InteractibleManager : NetworkBehaviour {
             }
         }
     }
-    /* DEPRECATED
-    public NetworkObject GetNetworkObjectByTag(string tag) {
-        NetworkObject[] networkObjects = GetComponentsInChildren<NetworkObject>();
-        foreach (NetworkObject networkObject in networkObjects) {
-            if (networkObject.gameObject.CompareTag(tag)) {
-                return networkObject;
-            }
-        }
-        Debug.LogError("Unable to find NetworkObject with tag " + tag);
-        return null;
-    }
-
-    // This sucks. Genuinely. But it's the only way to find the future object with the tag for now
-    public GameObject FindFutureObjectWithTag(string tag) {
-        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tag);
-        foreach (GameObject obj in taggedObjects) {
-            FutureObject futureObject = obj.GetComponent<FutureObject>();
-            if (futureObject != null) {
-                return obj;
-            }
-        }
-        Debug.LogError("Unable to find FutureObject with tag " + tag);
-        return null;
-    }
-
-    // ty copilot
-    public NetworkObject FindNetworkObjectById(ulong networkObjectId) {
-        NetworkObject[] networkObjects = GetComponentsInChildren<NetworkObject>();
-        foreach (NetworkObject networkObject in networkObjects) {
-            if (networkObject.NetworkObjectId == networkObjectId) {
-                return networkObject;
-            }
-        }
-        Debug.LogError("Unable to find NetworkObject with id " + networkObjectId);
-        return null;
-    }
-    
-
-    // Given an ID, find the network object from that ID
-    public NetworkObject FindNetworkObject(ulong networkObjectId) {
-        NetworkObject networkObject;
-        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(networkObjectId, out networkObject)) {
-            return networkObject;
-        } else {
-            return null;
-        }
-    }
-    */
-
-    private static GameObject InstantiatePastObject(PastObject pastObject, NetworkObject networkObject) {
-        return null;
-    }
-
-    private static GameObject InstantiateFutureObject()
-
-    private static GameObject InstantiatePastTelescope(PastObject pastObject, Variables variables, NetworkObject networkObject) {
-        TelescopeVariables telescopeVariables = (TelescopeVariables)variables;
-        GameObject Object = Instantiate(telescopeVariables.PastObjectPrefab);
-        PastTelescope pastTelescope = Object.GetComponent<PastTelescope>();
-        pastTelescope.networkObject = networkObject;
-        return Object;
-    }
-
-    private static GameObject InstantiateRadio(PastObject pastObject, Variables variables, NetworkObject networkObject) {
-        RadioVariables radioVariables = (RadioVariables)variables;
-        GameObject Object = Instantiate(radioVariables.PastObjectPrefab);
-        PastRadio pastRadio = Object.GetComponent<PastRadio>();
-        pastRadio.networkObject = networkObject;
-        return Object;
-    }
-
-    private static GameObject InstantiateSafe(PastObject pastObject, Variables variables, NetworkObject networkObject) {
-        SafeVariables safeVariables = (SafeVariables)variables;
-        GameObject Object = Instantiate(safeVariables.PastObjectPrefab);
-        PastSafe pastSafe = Object.GetComponent<PastSafe>();
-        pastSafe.networkObject = networkObject;
-        return Object;
-    }
 
     /* 
     RPC Calls here. Make sure each has an attribute specifying to whom it is sent to
@@ -147,8 +62,6 @@ public class InteractibleManager : NetworkBehaviour {
     public void InstantiatePastObjectRPC(NetworkObjectReference objectReference) {
         // TODO - Add transforms to all Instantiate calls 
         if (IsServer) { //Check if you are the past player here
-            // NetworkObject networkObject = FindNetworkObject(networkobjectid);
-
             objectReference.TryGet(out NetworkObject networkObject);
             GameObject Object = Instantiate(networkObject.gameObject.GetComponent<Variables>().PastObjectPrefab);
             Object.GetComponent<PastObject>().networkObject = networkObject;
@@ -159,14 +72,12 @@ public class InteractibleManager : NetworkBehaviour {
 
     [Rpc(SendTo.ClientsAndHost)]
     public void InstantiateFutureObjectRPC(NetworkObjectReference objectReference) {
-
         // TODO - Add transforms to all Instantiate calls 
         if (!IsServer) { //Check if you are the future player here
             objectReference.TryGet(out NetworkObject networkObject);
             GameObject Object = Instantiate(networkObject.gameObject.GetComponent<Variables>().FutureObjectPrefab);
             Object.GetComponent<FutureObject>().networkObject = networkObject;
             Object.Setup();
-            TransferOwnerServerRPC(objectReference, NetworkManager.Singleton.LocalClientId);
             TransferOwnerServerRPC(objectReference, NetworkManager.Singleton.LocalClientId);
         }
     }
