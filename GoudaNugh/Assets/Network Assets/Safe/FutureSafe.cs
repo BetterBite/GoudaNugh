808500c3cacbe51cd5a76724472296d84c4f949a
jacobrseaborn <jacobrseaborn@gmail.com>
2024-03-20 15:04:23 +0000

    fixed Gabriel's blunders

--- GoudaNugh/Assets/Network Assets/Safe/FutureSafe.cs
+++ GoudaNugh/Assets/Network Assets/Safe/FutureSafe.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class FutureSafe : FutureObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public override void Setup() {}
 }
--- GoudaNugh/Assets/Network Assets/Safe/PastSafe.cs
+++ GoudaNugh/Assets/Network Assets/Safe/PastSafe.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class PastSafe : PastObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public override void Setup() {}
 }
