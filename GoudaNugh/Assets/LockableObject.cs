72cd209a844aa9187b84c81aaa9d64bf60eec329
EdSu03 <edsusiyuan@gmail.com>
2024-04-03 20:13:55 +0100

    add drawers

--- GoudaNugh/Assets/LockableObject.cs
+++ GoudaNugh/Assets/LockableObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LockableObject : MonoBehaviour
@@ -7,14 +8,18 @@ public class LockableObject : MonoBehaviour
     private Rigidbody body;
     private Vector3 startingPosition;
     private Quaternion startingRotation;
-    private bool isLocked;
+    public bool isLocked;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        isLocked = false;
+        //isLocked = false;
         startingPosition = transform.position;
         startingRotation = transform.rotation;
         body = GetComponent<Rigidbody>();
+        if (isLocked)
+            Lock();
+        else
+            Unlock();
         
     }
 
@@ -36,28 +41,25 @@ public class LockableObject : MonoBehaviour
 
     public void Lock()
     {
-        if (!isLocked)
-        {
-            transform.position = startingPosition;
-            transform.rotation = startingRotation;
-            body.constraints = RigidbodyConstraints.FreezeAll;
-            isLocked = true;
-        }
+
+        transform.position = startingPosition;
+        transform.rotation = startingRotation;
+        body.constraints = RigidbodyConstraints.FreezeAll;
+        isLocked = true;
     }
 
     public void Unlock()
     {
-        if (isLocked)
-        {
-            body.constraints = RigidbodyConstraints.None;
-            isLocked = false;
-        }
+
+        body.constraints = RigidbodyConstraints.None;
+        isLocked = false;
+        body.AddForce(Vector3.forward*600000);
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
---
+++ GoudaNugh/Assets/Network Assets/Drawers/PastDrawers.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PastDrawers : PastObject
+{
+    // Start is called before the first frame update
+    public override void Setup()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
