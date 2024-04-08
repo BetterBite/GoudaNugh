f0f3c071b5fc29beef10a7de7e6c1b8e502d3ee2
EdSu03 <edsusiyuan@gmail.com>
2024-04-08 16:51:53 +0100

    sine wave network behaviour

--- GoudaNugh/Assets/Sinewave.cs
+++ GoudaNugh/Assets/Sinewave.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class Sinewave : MonoBehaviour
+public class Sinewave : NetworkBehaviour
 {
 
     public LineRenderer lr;
