e247eee713b22d5a2931c2770b06150fd94f2776
EdSu03 <edsusiyuan@gmail.com>
2024-04-09 15:39:55 +0100

    ummm

--- GoudaNugh/Assets/FutureGlobeLever.cs
+++ GoudaNugh/Assets/FutureGlobeLever.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 
-public class ContinuousRotatableCheck : MonoBehaviour
+public class FutureGlobeLever : MonoBehaviour
 {
     // Start is called before the first frame update
     //public int threshold;
@@ -12,9 +13,13 @@ public class ContinuousRotatableCheck : MonoBehaviour
     private HingeJoint joint;
     private int axis;
     private float rotation;
-    private bool grabbed;
+    public bool grabbed;
     public bool activated;
     private Quaternion startRot;
+
+    public float velocity;
+    public CounterMove counter;
+
     void Awake()
     {
         startRot = transform.localRotation;
@@ -58,11 +63,28 @@ public class ContinuousRotatableCheck : MonoBehaviour
     //}
 
     // Update is called once per frame
+
+    private void applyVelocity(float angle)
+    {
+
+        if (angle > 0.1 && angle < -0.1) velocity = 0f;
+        else
+        {
+            velocity = angle * 3;
+        }
+        counter.rotate(new Vector3(velocity, 0, 0));
+
+
+    }
+
     void Update()
     {
         if (grabbed)
         {
-            Debug.Log(transform.rotation.eulerAngles[axis]);
+            float angle = transform.rotation[axis];
+            //if (angle > 90) angle = -1 * angle + 180;
+            applyVelocity(angle);
+            //Debug.Log(transform.rotation[axis]);
 
             activated = (transform.rotation.eulerAngles[axis] < triggerAngle);
             //Debug.Log(activated);
