8b081b63f7bc3c84cfe89ddd2d55da687f94390a
EdSu03 <edsusiyuan@gmail.com>
2024-04-17 20:15:34 +0100

    added globe target

--- GoudaNugh/Assets/Network Assets/Globe/GlobeVariables.cs
+++ GoudaNugh/Assets/Network Assets/Globe/GlobeVariables.cs
@@ -6,7 +6,9 @@ using Unity.Netcode;
 public class GlobeVariables : Variables
 {
     public NetworkVariable<Vector3> rotation = new NetworkVariable<Vector3>();
-    
+    public NetworkVariable<Quaternion> targetHoriz = new NetworkVariable<Quaternion>();
+    public NetworkVariable<Quaternion> targetVert = new NetworkVariable<Quaternion>();   
+
     public void PastMove(Vector3 vec)
     {
         rotation.Value += vec;
@@ -16,6 +18,16 @@ public class GlobeVariables : Variables
     public void FutureMoveServerRpc(Vector3 vec) 
     {
         rotation.Value += vec;
+    }
+
+    private void Update()
+    {
+        targetHoriz.Value = Quaternion.Euler(0, targetHoriz.Value.y + 0.1f, 0);
+        float prog = Mathf.Sin(Time.timeSinceLevelLoad)/2 + 0.5f;
+        float vertRot = Mathf.Lerp(0, 90, prog);
+        targetVert.Value = Quaternion.Euler(0, 0, vertRot);
+        //vert.Rotate(0, 0, prog);
+
     }
 
 
