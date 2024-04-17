1dfc911c921d61422b7508b4d291bd0fb270ddb1
EdSu03 <edsusiyuan@gmail.com>
2024-04-17 02:32:36 +0100

    networked debris for vacuum

--- GoudaNugh/Assets/Debris.cs
+++ GoudaNugh/Assets/Debris.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class Debris : MonoBehaviour
+public class Debris : NetworkBehaviour
 {
     // Start is called before the first frame update
+    private bool IsPast;
+    public MeshRenderer mr;
     void Start()
     {
+        mr = GetComponent<MeshRenderer>();
+        mr.materials[0].color = Color.white;
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(0, 100, 100));
+        rb.AddForce(new Vector3(0, 50, 0));
     }
 
     // Update is called once per frame
