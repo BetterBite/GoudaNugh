95873384bc4b88ea499ec9d191ef91cde603c8ed
BetterBite <38665746+BetterBite@users.noreply.github.com>
2024-04-18 12:31:23 +0100

    added listener to futuresafe

--- GoudaNugh/Assets/Network Assets/Safe/FutureSafe.cs
+++ GoudaNugh/Assets/Network Assets/Safe/FutureSafe.cs
@@ -12,5 +12,14 @@ public class FutureSafe : FutureObject {
 
     public override void Setup() {
         Assert.IsNotNull(lockableObject, "Lockable object script has not been set for this instance of safe!");
+        networkObject.GetComponent<SafeVariables>().isLocked.OnValueChanged += OnIsLockedChanged;
+    }
+
+    private void OnIsLockedChanged(bool previousValue, bool newValue) {
+        if (newValue) {
+            lockableObject.Lock();
+        } else {
+            lockableObject.Unlock();
+        }
     }
 }
