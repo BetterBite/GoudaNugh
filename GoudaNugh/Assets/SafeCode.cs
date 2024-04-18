14a9b27440232b7e446365e85386ad54bd0f4d31
BetterBite <38665746+BetterBite@users.noreply.github.com>
2024-04-18 11:53:51 +0100

    Cleanup and fix

--- GoudaNugh/Assets/SafeCode.cs
+++ GoudaNugh/Assets/SafeCode.cs
@@ -5,40 +5,29 @@ using TMPro;
 using System;
 using Random = UnityEngine.Random;
 
-public class SafeCode : MonoBehaviour
-{
+public class SafeCode : MonoBehaviour {
     bool Locked = true;
-    public int[] code = { Random.Range(0,10), Random.Range(0,10), Random.Range(0,10)};
+    public int[] code = { 0, 0, 0};
     public TMP_Text[] content;
     public GameObject hinge;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Start() {
+        code[0] = Random.Range(0, 10);
+        code[1] = Random.Range(0, 10);
+        code[2] = Random.Range(0, 10);
         Debug.Log("Code generated: " + code[0] + code[1] + code[2]);
     }
 
-    public void CheckCode()
-    {
+    public void CheckCode() {
         int count = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            if (code[i] == Int32.Parse(content[i].text))
-            {
+        for (int i = 0; i < 3; i++) {
+            if (code[i] == Int32.Parse(content[i].text)) {
                 count++;
             } 
         }
-        if (count>2)
-        {
+        if (count>2) {
             Debug.Log("correct code");
             hinge.GetComponent<Hinge>().open();
         }
-       
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- GoudaNugh/Assets/SafeNumber.cs
+++ GoudaNugh/Assets/SafeNumber.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class SafeNumber : MonoBehaviour
-{
+public class SafeNumber : MonoBehaviour {
     public TMP_Text content;
     public int num;
     public GameObject safeCode;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Start() {
         num = Random.Range(0, 10);
         content.text = num.ToString();
     }
-
-    public void Poke()
-    {
+    public void Poke() {
         num = (num + 1) % 10;
         content.text = num.ToString();
         safeCode.GetComponent<SafeCode>().CheckCode();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
