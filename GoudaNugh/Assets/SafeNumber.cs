cc70a74c1dec57373941fdf3212cbdfcdad3f27f
EdSu03 <edsusiyuan@gmail.com>
2024-04-18 14:43:13 +0100

    safe up and down buttons

--- GoudaNugh/Assets/SafeNumber.cs
+++ GoudaNugh/Assets/SafeNumber.cs
@@ -12,9 +12,22 @@ public class SafeNumber : MonoBehaviour {
         num = Random.Range(0, 10);
         content.text = num.ToString();
     }
-    public void Poke() {
-        num = (num + 1) % 10;
+    public void Poke(bool up) {
+
+        if (up)
+        {
+            num = (num + 1) % 10;
+        }
+        else {
+            if (num == 0) num = 9;
+            else num = num - 1;
+        }
+        
+        Debug.Log(num);
         content.text = num.ToString();
         safeCode.CheckCode();
     }
+
+
+
 }
