3b9aa95ca1c1b01748d3b120f9941da4560e40a5
EdSu03 <edsusiyuan@gmail.com>
2024-04-23 20:31:21 +0100

    vase and book key

--- GoudaNugh/Assets/Book.cs
+++ GoudaNugh/Assets/Book.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class Book : MonoBehaviour
@@ -7,6 +8,8 @@ public class Book : MonoBehaviour
     private int vars;
     public Rigidbody leftBody;
     public Rigidbody rightBody;
+    public GameObject Compass;
+    public Transform compassSpawn;
     private bool isLocked = true;
 
     public void Unlock()
@@ -18,4 +21,10 @@ public class Book : MonoBehaviour
         leftBody.isKinematic = false;
         rightBody.isKinematic = false;
     }
+
+    public void SpawnCompass()
+    {
+        Instantiate(Compass, compassSpawn);
+    }
+
 }
