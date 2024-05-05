5c52f0175ca5b4c385fbcdb14483e28a5b8182c3
Maciej Braszczok <tc19422@bristol.ac.uk>
2024-05-05 20:35:21 +0100

    added countdown clock, first version, working

---
+++ GoudaNugh/Assets/Scripts/GameLoop/TimerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Vivox;
+using Unity.Services.Relay.Models;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.SceneManagement;
+using System;
+using TMPro;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] Image timerGraphic;
+    [SerializeField] float gameTimeInSeconds;
+
+    float maxGameTime;
+
+    private void Awake()
+    {
+        maxGameTime = gameTimeInSeconds;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimeInSeconds -= Time.deltaTime;
+
+        var updateTimerGraphicValue = gameTimeInSeconds / maxGameTime;
+
+        timerGraphic.fillAmount = updateTimerGraphicValue;
+
+        if (gameTimeInSeconds<=0)
+        {
+            Debug.Log("Loading Lobby");
+            NetworkManager.Singleton.SceneManager.LoadScene("LobbyScene", LoadSceneMode.Single);
+        }
+    }
+}
