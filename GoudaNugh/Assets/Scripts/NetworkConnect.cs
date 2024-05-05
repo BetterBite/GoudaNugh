162c4c9ea241ccd1c0d0d7af8fde68887c41d825
BetterBite <38665746+BetterBite@users.noreply.github.com>
2024-05-05 13:56:26 +0100

    truncate networkconnect

--- GoudaNugh/Assets/Scripts/NetworkConnect.cs
+++ GoudaNugh/Assets/Scripts/NetworkConnect.cs
@@ -13,55 +13,35 @@ using UnityEngine.SceneManagement;
 using System;
 using TMPro;
 
-public class NetworkConnect : MonoBehaviour
-{
+public class NetworkConnect : MonoBehaviour {
     public int maxConnection = 2;
     public UnityTransport transport;
     public string joinCode;
-
     public Transform p1Pos;
     public Transform p2Pos;
     public Transform rig;
     public LobbyUIHandler uiHandler;
     public TMP_InputField myTextMeshProInputJoinCode;
-
     [SerializeField]
     private Toggle VoiceToggle;
 
-
-
-    private async void Awake()
-    {
+    private async void Awake() {
         await UnityServices.InitializeAsync();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
-        //
-
-        VoiceToggle.onValueChanged.AddListener(delegate 
-            { VivoxToggle(VoiceToggle); });
-
+        VoiceToggle.onValueChanged.AddListener(delegate{VivoxToggle(VoiceToggle);});
     }
 
-    void VivoxToggle(Toggle voiceToggle)
-    {
+    void VivoxToggle(Toggle voiceToggle) {
         Debug.Log("Voice " + voiceToggle.isOn);
-
     }
 
-    public async void SetJoinCode()
-    {
+    public async void SetJoinCode() {
         joinCode = myTextMeshProInputJoinCode.text;
         //joinCode = joinCode.Trim();
-
     }
 
-
-    public async void Create()
-    {
-        // File -> Build Settings -> Scenes in Build -> assign to the function below a numer of a scene to get player to
-
-        // we don't want to load scene on create.
-        // SceneManager.LoadScene("BetaSceneMain");
+    public async void Create() {
         uiHandler.SetIsHost(true);
         uiHandler.SetState(1);
 
@@ -78,74 +58,31 @@ public class NetworkConnect : MonoBehaviour
         // display Start Game button
         uiHandler.SetJoinCodeForDisplay(newJoinCode);
         uiHandler.SetState(2);
-
-
-        // We don't want to move the player on create.
-        // rig.position = p1Pos.position;
-
-        // File -> Build Settings -> Scenes in Build -> assign to the function below a numer of a scene to get player to
-        //SceneManager.LoadScene("BetaSceneNetworkTest");
-        //StartGame();
-
     }
 
 
-        public async void Join()
-    {
-        // File -> Build Settings -> Scenes in Build -> assign to the function below a numer of a scene to get player to
-        // We don't want to load scene on join
-        // SceneManager.LoadScene("BetaSceneMain");
+    public async void Join() {
         uiHandler.SetIsHost(false);
         uiHandler.SetState(1);
-
-
-
         Debug.Log("Join code being used: " + joinCode);
-
         // Ensure joinCode is not null or empty
-        if (string.IsNullOrWhiteSpace(joinCode))
-        {
+        if (string.IsNullOrWhiteSpace(joinCode)) {
             Debug.LogError("Join code is empty or null. Please set a valid join code.");
             return; // Exit the method to avoid making a request with invalid join code
         }
-
-
         JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-
         transport.SetClientRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port,
            allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData, allocation.HostConnectionData);
-
         NetworkManager.Singleton.StartClient();
 
         // show "Waiting for Host to Start game"
         uiHandler.SetState(2);
-
-        // We don't want to move player on join
-        // rig.position = p2Pos.position;
-
-        // File -> Build Settings -> Scenes in Build -> assign to the function below a numer of a scene to get player to
-        //SceneManager.LoadScene("BetaSceneNetworkTest");
     }
 
-    public void StartGame() 
-    {
-        // TODO: Implement starting the game
-        // This method is called by the Host when clicking the "Start Game" button in the lobby.
-
-        // SceneManager.LoadScene("BetaSceneMain");
+    public void StartGame() {
         Debug.Log("Starting Game");
         NetworkManager.Singleton.SceneManager.LoadScene("BetaSceneSchoolsDayFixes", LoadSceneMode.Single);
-
-
-
-        InteractibleManager.Singleton.OnSceneLoad();
-        
-        //NetworkSceneManager.OnSceneEvent.
-        
-
+        // InteractibleManager.Singleton.OnSceneLoad();
+        // Why is this here if its being done by the network manager??? We are double loading the scene
     }
-
-
-
 }
-
