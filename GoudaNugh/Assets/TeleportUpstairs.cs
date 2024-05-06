98528213b3d151162d5959cfecdf07fdb4658df0
Maciej Braszczok <tc19422@bristol.ac.uk>
2024-05-06 17:49:00 +0100

    fixed arrow

--- GoudaNugh/Assets/TeleportUpstairs.cs
+++ GoudaNugh/Assets/TeleportUpstairs.cs
@@ -10,19 +10,26 @@ public class TeleportUpstairs : MonoBehaviour
 
     public FadeScreen fadeScreen;
 
-    public void teleportTo(Transform transform) {
+    public void teleportTo(Transform transform)
+    {
         StartCoroutine(fadeCoroutine(transform));
     }
 
-    IEnumerator fadeCoroutine(Transform transform) {
-        fadeScreen.Fade(0,1);
+    IEnumerator fadeCoroutine(Transform transform)
+    {
+        fadeScreen.Fade(0, 1);
         yield return new WaitForSeconds(fadeScreen.FadeDuration);
 
         // move player to transform
-        player.position = upstairs.position;
+        //player.position = upstairs.position;
+        // Create a new Vector3 for the player's position with x and z from player.position and y from upstairs.position
+        Vector3 newUpstairs = new Vector3(player.position.x, upstairs.position.y, player.position.z);
+
+        // Move player to the new position
+        player.position = newUpstairs;
 
         // fade from black
-        fadeScreen.Fade(1,0); 
+        fadeScreen.Fade(1, 0);
     }
 }
 
